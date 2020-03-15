ba697c6beef4b38bd1e54762692bed87414bbb5b
jefferoonie <jefferoonie@gmail.com>
2020-03-15 18:03:50 -0500

    Update wionToggle.py

--- wionToggle.py
+++ wionToggle.py
@@ -1,3 +1,6 @@
+## Using GPIO 17 to toggle the WiOn device on and off.
+## WiOn trigger toggles Hight to Low
+
 ## Import GPIO library
 import RPi.GPIO as GPIO
 import time
@@ -8,4 +11,4 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 GPIO.output(17, True)
 time.sleep(1)
-GPIO.output(17, False)
+GPIO.output(17, False
